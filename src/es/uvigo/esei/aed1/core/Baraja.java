7bbd30e8939f6bcffdd8105fb33d46840ac54871
VascoDeGamaEsp <jose.a.ferro.g@gmail.com>
2023-04-06 13:40:10 +0200

    Cambié EnlazadaBaraja para que tuviera un centinela. Ignorad el UltimaCartaExcepcion. (Jose)

--- src/es/uvigo/esei/aed1/core/Baraja.java
+++ src/es/uvigo/esei/aed1/core/Baraja.java
@@ -13,18 +13,33 @@ import pila.PilaVaciaExcepcion;
 
 public class Baraja {
 
-    private Pila baraja;
+    EnlazadaBaraja  baraja = new EnlazadaBaraja();
 
-    public Baraja() {
+    public Baraja() throws PilaVaciaExcepcion{ 
+        
+        String[] palo = {"oros", "espadas", "bastos", "copas"}; 
+        
+        for (int i = 0; i < palo.length; i++) { 
+            for (int j = 1; j <= 12; j++) { 
+                                            
+                this.insertarCarta( new NodoCarta( new Carta( j, palo[i]), null) );
+
+            }
+            
+        }
+        
     }
 
-    public Pila getBaraja() {
+    public EnlazadaBaraja getBaraja() {
         return baraja;
     }
 
-    public void setBaraja(Pila baraja) {
+    public void setBaraja(EnlazadaBaraja baraja) {
         this.baraja = baraja;
     }
-
     
+    public void insertarCarta(NodoCarta carta){
+        baraja.push(carta);
+    }
+     
 }
