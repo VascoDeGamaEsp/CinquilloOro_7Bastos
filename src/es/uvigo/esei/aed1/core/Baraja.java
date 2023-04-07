ecf6f251644457e476b7693f651b0a79d64faa59
VascoDeGamaEsp <jose.a.ferro.g@gmail.com>
2023-04-07 19:05:29 +0200

    Cree en metodo barajar en la clase Baraja y cree las constantes NUMERO_PALOS y CARTAS_POR_PALO (Jose)

--- src/es/uvigo/esei/aed1/core/Baraja.java
+++ src/es/uvigo/esei/aed1/core/Baraja.java
@@ -7,12 +7,14 @@
 package es.uvigo.esei.aed1.core;
 
 import cola.Cola;
+import java.util.Random;
 import pila.Pila;
 import pila.PilaVaciaExcepcion;
 
 
 public class Baraja {
-
+    final static int CARTAS_POR_PALO = 12;
+    final static int NUMERO_PALOS = 4;
     EnlazadaBaraja  baraja = new EnlazadaBaraja();
 
     public Baraja() throws PilaVaciaExcepcion{ 
@@ -20,12 +22,38 @@ public class Baraja {
         String[] palo = {"oros", "espadas", "bastos", "copas"}; 
         
         for (int i = 0; i < palo.length; i++) { 
-            for (int j = 1; j <= 12; j++) { 
+            for (int j = 1; j <= CARTAS_POR_PALO; j++) { 
                                             
                 this.insertarCarta( new NodoCarta( new Carta( j, palo[i]), null) );
 
             }
             
+        }
+        
+    }
+    
+    public void barajar(){
+        //atributos
+        int NUMERO_CARTAS = CARTAS_POR_PALO * NUMERO_PALOS;
+        int posicionAIntroducir;
+        
+        
+        Carta[] arrayCarta = new Carta[NUMERO_CARTAS];
+        Random aleatorio = new Random(System.currentTimeMillis());
+        
+        
+        //inicio
+        for (int i = 0; i < arrayCarta.length; i++) {
+            arrayCarta[i] = baraja.pop().getElemento();    
+        }
+        
+        int i = NUMERO_CARTAS - 1;
+        while ( i >= 0 ) {
+            
+            posicionAIntroducir = aleatorio.nextInt(NUMERO_CARTAS);
+            baraja.push( new NodoCarta( arrayCarta[posicionAIntroducir], null));
+            arrayCarta[posicionAIntroducir] = arrayCarta[i];
+            i--;
         }
         
     }
@@ -40,6 +68,19 @@ public class Baraja {
     
     public void insertarCarta(NodoCarta carta){
         baraja.push(carta);
+    }
+    
+    public NodoCarta sacarCarta(){
+        return baraja.pop();
+    }
+    
+    private void mostrarBaraja(){
+        int i = 1;
+        while (!baraja.esVacio()) {
+            System.out.println(baraja.pop().mostrarCarta());
+            System.out.println(i);
+            i++;
+        }
     }
      
 }
